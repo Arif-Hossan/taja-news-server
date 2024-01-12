9217bb38b2e5b6c22f52e924b26defc36257b1f5
Arif Hossan <arif.hossan.eu@gmail.com>
2024-01-12 23:23:34 +0600

    data load from server

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.port || 5000;
 const cors = require('cors');
 // importing categories data from the directory
 const categories = require('./data/categories.json');
+// importing all news data from the directory
+const news = require('./data/news.json');
 app.use(cors());
 
 app.get('/',(req,res)=>{
@@ -14,6 +16,27 @@ app.get('/',(req,res)=>{
 app.get('/categories',(req,res)=>{
     res.send(categories)
 })
+// all news load
+app.get('/news',(req,res)=>{
+    res.send(news);
+})
+// category wise news data load
+app.get('/category/:id',(req,res)=>{
+    const id = req.params.id;
+    // console.log( id);
+    const categoryNews = news.filter(n => n.category_id === id);
+    res.send(categoryNews);
+})
+// id wise single news load
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    // console.log(typeof id);
+    // as id is a string thats why we need to convert it in a number here
+    // as we compare it using triple equal
+    const singleNews = news.find(n=> parseInt(n._id) === parseInt(id));
+    res.send(singleNews);
+})
+
 app.listen(port,()=>{
     console.log('Taja hoye jaw');
 })
